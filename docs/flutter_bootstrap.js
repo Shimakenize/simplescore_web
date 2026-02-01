2f865c3c2a1ec8f2c7fa524af3bda8f0a0dbcbc5
Shimakenize <smkn@mti.biglobe.ne.jp>
2026-02-01 10:10:18 +0900

    redeploy stable main.dart

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2404944393"
+    serviceWorkerVersion: "3453384093"
   }
 });
